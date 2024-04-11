32b026f39ad0767ab2835f5bc66c820b78977ddf
Shuyang Sun <shuyangsun10@gmail.com>
2024-04-10 23:59:03 -0400

    Bug fix.

--- mmdet/visualization/local_visualizer.py
+++ mmdet/visualization/local_visualizer.py
@@ -90,12 +90,12 @@ class DetLocalVisualizer(Visualizer):
         mask_color: Optional[Union[str, Tuple[int]]] = None,
         line_width: Union[int, float] = 3,
         alpha: float = 0.5,
-        draw_bboxes: bool = False,  # MARK: SSML_CUSTOM
+        should_draw_bboxes: bool = True,  # MARK: SSML_CUSTOM
     ) -> None:
         super().__init__(
             name=name, image=image, vis_backends=vis_backends, save_dir=save_dir
         )
-        self.draw_bboxes = draw_bboxes
+        self.should_draw_bboxes = should_draw_bboxes
         self.bbox_color = bbox_color
         self.text_color = text_color
         self.mask_color = mask_color
@@ -128,7 +128,11 @@ class DetLocalVisualizer(Visualizer):
         """
         self.set_image(image)
 
-        if self.draw_bboxes and "bboxes" in instances and instances.bboxes.sum() > 0:
+        if (
+            self.should_draw_bboxes
+            and "bboxes" in instances
+            and instances.bboxes.sum() > 0
+        ):
             bboxes = instances.bboxes
             labels = instances.labels
 
